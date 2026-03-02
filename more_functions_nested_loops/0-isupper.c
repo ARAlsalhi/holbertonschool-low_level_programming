9283eb150619986af89cdecdaafd1954c9c374f1
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-03-02 11:00:59 +0000

    new fix for 0-isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,11 @@
 #include "main.h"
+
+/**
+*this funtion is mean 
+*check for the value if > or < A 
+* return value 1 or 0 besd on the t/f 
+**/
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
