076fdb324a7b8b22b22fe5f82e51513532afca60
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-03-03 08:53:14 +0000

    add mul funtion

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul- is the multyplayer for two number a,b
+ * @a: it is the first intger
+ * @b: it is the second intger
+ *
+ * Return: it hold the result tha be in mul.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
