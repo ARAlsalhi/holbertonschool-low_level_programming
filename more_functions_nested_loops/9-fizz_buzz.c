10b8827d937aff007813845d539e09ca45762e84
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-03-04 10:21:01 +0000

    add the fizz buzz solve

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ * followed by Fizz, Buzz, or FizzBuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+	if (i % 15 == 0)
+	printf("FizzBuzz");
+
+	else if (i % 5 == 0)
+	printf("Fizz");
+
+	else if (i % 5 == 0)
+	printf("Buzz");
+
+	else
+	printf("%d", i);
+
+	if (i != 100)
+	printf(" ");
+}
+	printf("\n");
+	return (0);
+}
