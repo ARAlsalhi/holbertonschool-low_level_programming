18259c8e0b8858b5fc98cca3075d889b8d70eefb
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-03-04 08:26:05 +0000

    add print_line_funtion

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print line - draws a stright line in the terminal
+* @n: number of tims the char '_' shpuld be print
+*
+* Retrun: void
+*/
+void print_line(int n)
+{
+int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
