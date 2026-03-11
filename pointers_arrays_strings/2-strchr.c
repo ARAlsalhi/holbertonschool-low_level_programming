05eaacc5d08e671bdadfbbc854242b6481386e9a
ARlsalhi <aalsalhi.eng@gail.com>
2026-03-11 07:47:21 +0000

    Add _strchr funtion

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *_strchr - it is search the char in the string by using array
+* @s: the string which we will search the char on it
+* @c: it is the char which we need it
+*
+* Return:a pointer to the first occurrence of the character c in the string s
+*
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+	}
+	return (NULL);
+
+}
