fc7d36330bc3fffdb63b6260129c123f69b728d7
ARlsalhi <aalsalhi.eng@gail.com>
2026-04-04 08:36:01 +0000

    Add int **alloc_grid funtion

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,21 +25,26 @@ if (grid == NULL)
 
 for (i = 0; i < height; i++)
 {
-	grid[i] = malloc(sizeof(int) * width);
+grid[i] = malloc(sizeof(int) * width);
 
-if (grid[i] == NULL)
+	if (grid[i] == NULL)
 
 {
-for (j = 0; j < i; j++)
+	for (j = 0; j < i; j++)
+	{
 	free(grid[j]);
+	}
 
 	free(grid);
 	return (NULL);
 	}
 
 for (j = 0; j < width; j++)
+	{
 	grid[i][j] = 0;
 	}
+
+}
 
 	return (grid);
 
