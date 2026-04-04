46cc7eb458493b8bc1a81774519f36598c8fa9a9
ARlsalhi <aalsalhi.eng@gail.com>
2026-04-04 16:04:28 +0000

    Add the new funtion to dog.h

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,10 +1,11 @@
 #include "dog.h"
+#include <stddef.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-if (d ==NULL)
-	retrun;
+if (d == NULL)
+return;
 
 d->name = name;
 d->age = age;
