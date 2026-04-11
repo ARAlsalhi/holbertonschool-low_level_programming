e58192ec11219f8c5a3a12f7b9c3a09055a5cf21
ARlsalhi <aalsalhi.eng@gail.com>
2026-04-11 15:08:28 +0000

    last fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -7,16 +7,6 @@
  *
  * Return: 0 on success
  */
-
-#include "3-calc.h"
-
-/**
- * main - performs simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
 {
 	int num1, num2;
