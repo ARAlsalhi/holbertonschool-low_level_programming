e6b72af61df723f51864236329d3ce228463b9af
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-24 01:10:58 +0300

    fix betty_style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,6 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -10,29 +9,30 @@
  *
  * Return: address of the new node, or NULL if it failed
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *tmpNode = malloc(sizeof(list_t));
+	list_t *tmpNode;
 	unsigned int len = 0;
 
-if (tmpNode == NULL)
-	return (NULL);
+	tmpNode = malloc(sizeof(list_t));
+	if (tmpNode == NULL)
+	{
+		return (NULL);
+	}
 
 	tmpNode->str = strdup(str);
-	if (new_node->str == NULL)
+	if (tmpNode->str == NULL)
 	{
 		free(tmpNode);
 		return (NULL);
 	}
 
-while (str[len] != '\0')
-{
-	len++;
+	while (str[len] != '\0')
+		len++;
+
 	tmpNode->len = len;
 	tmpNode->next = *head;
 	*head = tmpNode;
-}
 
-return (tmpNode);
+	return (tmpNode);
 }
