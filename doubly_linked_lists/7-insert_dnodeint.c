024824227eb83b47daea6ccd523fac41ee548e0f
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-24 02:51:44 +0300

    Add the insert function

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given index
+ * @h: pointer to the head of the list
+ * @idx: index where the new node should be added
+ * @n: value to store in the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp;
+	dlistint_t *new_node;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	tmp = *h;
+
+	while (tmp != NULL && count < idx - 1)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+
+	if (tmp == NULL)
+		return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+
+	tmp->next->prev = new_node;
+	tmp->next = new_node;
+
+	return (new_node);
+}
