cd2ce6eb87d6b07f46b1638cad4d8d0451b0f3a4
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-24 01:06:42 +0300

    Add new funtion add_node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to duplicate
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmpNode = malloc(sizeof(list_t));
+	unsigned int len = 0;
+
+if (tmpNode == NULL)
+	return (NULL);
+
+	tmpNode->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(tmpNode);
+		return (NULL);
+	}
+
+while (str[len] != '\0')
+{
+	len++;
+	tmpNode->len = len;
+	tmpNode->next = *head;
+	*head = tmpNode;
+}
+
+return (tmpNode);
+}
