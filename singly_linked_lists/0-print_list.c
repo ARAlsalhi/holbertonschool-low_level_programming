9d6da72188a550f66bb20d39bda83115df2f24f5
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-24 00:26:37 +0300

    Fix the 0_print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
 #include "lists.h"
-
-
+#include <stdio.h>
 
 /**
- * print_list - prints all elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: pointer to the first node
  *
  * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-
-	size_t nodeCountNum = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		list_t tmpNod = malic
-
 		if (h->str == NULL)
-		{
-			printf("print[0] (nil)\n");
-		}
-
+			printf("[0] (nil)\n");
 		else
-		{
-			printf("-> %lu elements %s\n", h->str, h->len);
-			h = h->next;
-			nodeCountNum++;
-		}
+			printf("[%u] %s\n", h->len, h->str);
 
-		return (nodeCountNum);
+		h = h->next;
+		count++;
 	}
+
+	return (count);
 }
