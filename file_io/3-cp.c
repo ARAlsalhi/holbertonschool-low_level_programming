5a694b87124cb1e691b8a55a906b5a72775d19a5
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-25 22:04:10 +0300

    Fix betty-style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,6 +21,38 @@ void close_file(int fd)
 }
 
 /**
+ * copy_file - copies content from one file to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: source file name
+ * @file_to: destination file name
+ *
+ * Return: Nothing
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t read_count, write_count;
+	char buffer[BUFFER_SIZE];
+
+	read_count = read(fd_from, buffer, BUFFER_SIZE);
+	while (read_count > 0)
+	{
+		write_count = write(fd_to, buffer, read_count);
+		if (write_count == -1 || write_count != read_count)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+		read_count = read(fd_from, buffer, BUFFER_SIZE);
+	}
+	if (read_count == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: arguments count
  * @argv: arguments vector
@@ -30,8 +62,6 @@ void close_file(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t read_count, write_count;
-	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -54,29 +84,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	read_count = read(fd_from, buffer, BUFFER_SIZE);
-	while (read_count > 0)
-	{
-		write_count = write(fd_to, buffer, read_count);
-
-		if (write_count == -1 || write_count != read_count)
-		{
-			close_file(fd_from);
-			close_file(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-
-		read_count = read(fd_from, buffer, BUFFER_SIZE);
-	}
-
-	if (read_count == -1)
-	{
-		close_file(fd_from);
-		close_file(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	copy_file(fd_from, fd_to, argv[1], argv[2]);
 
 	close_file(fd_from);
 	close_file(fd_to);
