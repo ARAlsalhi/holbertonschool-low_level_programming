0fee52e53b874fc9efb4af399cc100e95b829e4e
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-29 16:19:15 +0300

    Fix the error

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,27 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "hash_tables.h"
 
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
  *
- * Return: pointer to the new hash table, or NULL if failed
+ * Return: pointer to the newly created hash table, or NULL if failed
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
 
-
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-
 	ht->size = size;
-
 
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
@@ -29,7 +23,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
-
 
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
